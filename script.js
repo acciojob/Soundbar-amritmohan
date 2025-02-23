774893630a713ae5f85447e601974225e9a64a16
@amritmohan <amritmohan2284@gmail.com>
2025-02-23 18:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 //your JS code here. If required.
 
-let sounds = ["s1", "s2", "s3", "s4", "s5", "s6"];
+document.addEventListener("DOMContentLoaded", () => {
+	let sounds = ["s1", "s2", "s3", "s4", "s5", "s6"];
+	let containerButton = document.getElementById("buttons");
 
-let containerButton = document.getElementById("buttons");
+	sounds.forEach(sound => {
+		let btn = document.createElement("button");
+		btn.classList.add("btn");
+		btn.innerText = sound;
 
-sounds.forEach(sound => {  
-	let btn = document.createElement("button");
-	btn.classList.add("btn");
-	btn.innerText = sound;
+		btn.addEventListener("click", () => {
+			stopAllSounds();
+			document.getElementById(sound).play();
+		});
 
-	btn.addEventListener("click", () => {
-		stopAllSounds();
-		document.getElementById(sound).play();
+		containerButton.appendChild(btn);
 	});
-	containerButton.appendChild(btn);
-});
 
-sounds.forEach(sound => {  
-	let audio = document.createElement("audio");
-	audio.id = sound;
-	audio.src = `sounds/${sound}.mp3`;
-	document.body.appendChild(audio);
-});
-
-document.querySelector(".stop").addEventListener("click", stopAllSounds);
-
-function stopAllSounds() {
-	sounds.forEach(sound => {  
-		let audio = document.getElementById(sound);
-		audio.pause();
-		audio.currentTime = 0;
+	sounds.forEach(sound => {
+		let audio = document.createElement("audio");
+		audio.id = sound;
+		audio.src = `sounds/${sound}.mp3`;
+		document.body.appendChild(audio);
 	});
-}
+
+	document.querySelector(".stop").addEventListener("click", stopAllSounds);
+
+	function stopAllSounds() {
+		sounds.forEach(sound => {
+			let audio = document.getElementById(sound);
+			if (audio) {
+				audio.pause();
+				audio.currentTime = 0;
+			}
+		});
+	}
+});
